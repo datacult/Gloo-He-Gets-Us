2bd76fb0d1c20711fd4281e62b44e895881a89e9
neil-oliver <neiloliverphoto@gmail.com>
2023-09-26 12:00:52 -0400

    Update metro-area-filtering.js

--- metro-area-filtering.js
+++ metro-area-filtering.js
@@ -1,3 +1,5 @@
+console.log("metro-area-filtering.js loaded")
+
 // Populate the Metro Area dropdown using the values stored in an external CSV
 var dropdown = $("#metro-area-dropdown");
 
@@ -16,16 +18,16 @@ $.ajax({
 
     // add event listener to dropdown after values have been added
     $("#metro-area-dropdown a").on("click", function (event) {
-      
+
       // make the dropdown close after a selection is made
       $(".w-dropdown").trigger("w-close");
       event.preventDefault();
-      
+
       // grab the selected item and create a filter object
       var selectedLink = $(this).text();
       console.log("Selected : " + selectedLink);
       var filter = [{ "column": "MSA", "operand": "IN", "values": [selectedLink] }]
-      
+
       // loop through all iframes on the page and update the src with the filter
       $("iframe").each(function () {
         if ($(this).attr("src").search("domo") != -1) {
